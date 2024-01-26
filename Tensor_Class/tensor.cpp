64a679b5ba43d41dd947812944a7c966a50c9c48
No <lockmecar@gmail.com>
2024-01-26 16:20:26 +0300

    Изменено поле хранения на двумерный дин. массив, изменены конструктор и деструктор, добавлен счетчик обьектотв класса.

--- Tensor_Class/tensor.cpp
+++ Tensor_Class/tensor.cpp
@@ -2,15 +2,30 @@
 
 void tensor::print()
 {
-
     for (int i = 0; i < tensor::size; i++) 
     {
         for (int j = 0; j < tensor::size; j++)
             std::cout << " " << tensor::matrix[i][j];
         std::cout << std::endl;
     }
+}
 
+size_t& tensor::count()
+{
+    static size_t c = 0;
+    return c;
+}
 
+tensor::tensor(int size_of_matrix)
+{
+    tensor::count()++;
+    tensor::size = size_of_matrix;
+    tensor::matrix = new float* [size_of_matrix];
+    for (int i = 0; i < size_of_matrix; i++)
+    {
+        matrix[i] = new float[size_of_matrix];
+    }
+    std::cout << "Object created. " << "Count: " << tensor::count() << std::endl;
 }
 
 size_t& tensor::count()
