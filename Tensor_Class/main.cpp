92bdc1d03818bf9a12e2239281244b47ec600605
No <lockmecar@gmail.com>
2024-01-26 16:32:08 +0300

    Revert "Изменено поле хранения на двумерный дин. массив, изменены конструктор и деструктор, добавлен счетчик обьектотв класса."
    This reverts commit 6c6f784dc596e8bd4dd1cc40181d5f6e40e7e8bd.

--- Tensor_Class/main.cpp
+++ Tensor_Class/main.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 int main()
 {
-    setlocale(0, "");
+	tensor tensor_1(6);
 
+	tensor_1.array[0][0] = 1;
+	tensor_1.array[3][3] = 2;
+	tensor_1.print();
 
-
-    return 0;
+	return 0;
 }
--- Tensor_Class/tensor.h
+++ Tensor_Class/tensor.h
@@ -4,13 +4,14 @@
 class tensor
 {
 private:
-	int size;        // Размер массива
-	float** matrix;  // Двумерный динамический массив
-	size_t& count(); // Счетчик обьектов
+	
 
 public:
 	tensor(int size); // Конструктор с указанием размера
-	~tensor();        // Деструктор
-	void print();     // Вывод на консоль
+	void print();
 
+	short size;
+	float array[100][100] = { 0 };
+
+	
 };
