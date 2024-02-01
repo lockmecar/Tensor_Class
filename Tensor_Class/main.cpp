f748488c10db49d2aaa99d95fa0b5d3aa3a17b74
No <lockmecar@gmail.com>
2024-02-01 16:28:57 +0300

    Выполнено слияние с testing.

--- Tensor_Class/main.cpp
+++ Tensor_Class/main.cpp
@@ -8,14 +8,8 @@ int main()
 
 	try
 	{
-		tensor a(3, "a");
-		a.set_object_of_matrix(1, 1, 50);
-		tensor b(3,"b");
-		b.set_object_of_matrix(1, 1, 50);
-		tensor c(3, "c");
-		c = a + b;
-		c.print();
-		a.print();
+		
+
 	}
 	catch(length_error& ex)
 	{
