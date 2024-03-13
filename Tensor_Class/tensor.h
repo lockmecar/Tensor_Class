0f718e137d768d1d165b302ebc8a94f2a7c1e188
nic gor <nic14246@mail.ru>
2024-03-13 14:44:59 +0300

    начало 3х мерного масива

--- Tensor_Class/tensor.h
+++ Tensor_Class/tensor.h
@@ -57,3 +57,56 @@ public:
 	void fileout10(std::string b);           
 	void fileout16(std::string b);        
 };
+
+class tensor3
+{
+private:
+	std::string name;// Имя объекта класса
+	int size_x;
+	int size_y;      // Размер массива
+	int size_z;
+	float** matrix;  // Двумерный динамический массив
+	size_t& count(); // Счетчик обьектов
+	int rand_seed = 1;
+
+public:
+	tensor3(int size_x, int size_y, std::string name, char mode);
+	tensor3(int size_x, int size_y, std::string name); // Конструктор с указанием размера
+	tensor3(const tensor& copied_obj);                 // Конструктор копирования
+	~tensor3();                                        // Деструктор
+	void print();                                     // Вывод на консоль
+	void set_object_of_matrix(int x, int y, float value); // Сеттер для конкретного значения в матрице
+	void clear();
+
+	void ElemFunc(float (*func)(float));       // Элементарная функция над каждым элементом
+
+	float operator() (unsigned x, unsigned y); // Геттер для конкретного значения в матрице
+
+	union ftoul
+	{
+		float input;
+		unsigned output;
+	};
+
+	std::vector<float> matrix_to_vector();
+
+	tensor operator+ (const tensor& b) const;
+	tensor& operator+= (const tensor& b);
+
+	tensor& operator = (const tensor& b);
+
+	tensor operator- (const tensor& b)const;
+	tensor& operator-= (const tensor& b);
+
+	tensor operator* (const tensor& b) const;
+	tensor& operator*= (const tensor& b);
+
+	tensor operator^ (const float& b) const;
+	tensor& operator^= (const float& b);
+
+	tensor transp();            // Транспонирование
+	tensor abs();               // Модуль (возможно не нужен)
+	tensor sqrt();              // Квадратный корень
+	void fileout10(std::string b);
+	void fileout16(std::string b);
+};
