5a39f70d175b4322e015dda073111725997ca012
nic gor <nic14246@mail.ru>
2024-08-22 18:13:39 +0300

    найдена проблема(ввод данных из файла) + сделал ввод чисел через txt для тензера

--- Tensor_Class/main.cpp
+++ Tensor_Class/main.cpp
@@ -1,47 +1,32 @@
 #include <iostream>
 #include "tensor.h"
+#include "fstream"
 
 using namespace std;
 
 
 void filldata(Ten3D &a, Ten3D &filter)
 {
-	a.set_object_of_matrix(0, 0, 0, 0); a.set_object_of_matrix(1, 0, 0, 0); a.set_object_of_matrix(2, 0, 0, 0); a.set_object_of_matrix(3, 0, 0, 0); a.set_object_of_matrix(4, 0, 0, 0); a.set_object_of_matrix(5, 0, 0, 0); a.set_object_of_matrix(6, 0, 0, 0);
-	a.set_object_of_matrix(0, 1, 0, 0); a.set_object_of_matrix(1, 1, 0, 0); a.set_object_of_matrix(2, 1, 0, 1); a.set_object_of_matrix(3, 1, 0, 1); a.set_object_of_matrix(4, 1, 0, 2); a.set_object_of_matrix(5, 1, 0, 2); a.set_object_of_matrix(6, 1, 0, 0);
-	a.set_object_of_matrix(0, 2, 0, 0); a.set_object_of_matrix(1, 2, 0, 0); a.set_object_of_matrix(2, 2, 0, 1); a.set_object_of_matrix(3, 2, 0, 1); a.set_object_of_matrix(4, 2, 0, 0); a.set_object_of_matrix(5, 2, 0, 0); a.set_object_of_matrix(6, 2, 0, 0);
-	a.set_object_of_matrix(0, 3, 0, 0); a.set_object_of_matrix(1, 3, 0, 1); a.set_object_of_matrix(2, 3, 0, 1); a.set_object_of_matrix(3, 3, 0, 0); a.set_object_of_matrix(4, 3, 0, 1); a.set_object_of_matrix(5, 3, 0, 0); a.set_object_of_matrix(6, 3, 0, 0);
-	a.set_object_of_matrix(0, 4, 0, 0); a.set_object_of_matrix(1, 4, 0, 1); a.set_object_of_matrix(2, 4, 0, 0); a.set_object_of_matrix(3, 4, 0, 1); a.set_object_of_matrix(4, 4, 0, 1); a.set_object_of_matrix(5, 4, 0, 1); a.set_object_of_matrix(6, 4, 0, 0);
-	a.set_object_of_matrix(0, 5, 0, 0); a.set_object_of_matrix(1, 5, 0, 0); a.set_object_of_matrix(2, 5, 0, 2); a.set_object_of_matrix(3, 5, 0, 0); a.set_object_of_matrix(4, 5, 0, 1); a.set_object_of_matrix(5, 5, 0, 0); a.set_object_of_matrix(6, 5, 0, 0);
-	a.set_object_of_matrix(0, 6, 0, 0); a.set_object_of_matrix(1, 6, 0, 0); a.set_object_of_matrix(2, 6, 0, 0); a.set_object_of_matrix(3, 6, 0, 0); a.set_object_of_matrix(4, 6, 0, 0); a.set_object_of_matrix(5, 6, 0, 0); a.set_object_of_matrix(6, 6, 0, 0);
+	ifstream data1("data.txt");
 
-	a.set_object_of_matrix(0, 0, 1, 0); a.set_object_of_matrix(1, 0, 1, 0); a.set_object_of_matrix(2, 0, 1, 0); a.set_object_of_matrix(3, 0, 1, 0); a.set_object_of_matrix(4, 0, 1, 0); a.set_object_of_matrix(5, 0, 1, 0); a.set_object_of_matrix(6, 0, 1, 0);
-	a.set_object_of_matrix(0, 1, 1, 0); a.set_object_of_matrix(1, 1, 1, 1); a.set_object_of_matrix(2, 1, 1, 1); a.set_object_of_matrix(3, 1, 1, 1); a.set_object_of_matrix(4, 1, 1, 2); a.set_object_of_matrix(5, 1, 1, 0); a.set_object_of_matrix(6, 1, 1, 0);
-	a.set_object_of_matrix(0, 2, 1, 0); a.set_object_of_matrix(1, 2, 1, 0); a.set_object_of_matrix(2, 2, 1, 2); a.set_object_of_matrix(3, 2, 1, 1); a.set_object_of_matrix(4, 2, 1, 1); a.set_object_of_matrix(5, 2, 1, 2); a.set_object_of_matrix(6, 2, 1, 0);
-	a.set_object_of_matrix(0, 3, 1, 0); a.set_object_of_matrix(1, 3, 1, 1); a.set_object_of_matrix(2, 3, 1, 2); a.set_object_of_matrix(3, 3, 1, 0); a.set_object_of_matrix(4, 3, 1, 0); a.set_object_of_matrix(5, 3, 1, 2); a.set_object_of_matrix(6, 3, 1, 0);
-	a.set_object_of_matrix(0, 4, 1, 0); a.set_object_of_matrix(1, 4, 1, 0); a.set_object_of_matrix(2, 4, 1, 2); a.set_object_of_matrix(3, 4, 1, 1); a.set_object_of_matrix(4, 4, 1, 2); a.set_object_of_matrix(5, 4, 1, 1); a.set_object_of_matrix(6, 4, 1, 0);
-	a.set_object_of_matrix(0, 5, 1, 0); a.set_object_of_matrix(1, 5, 1, 2); a.set_object_of_matrix(2, 5, 1, 0); a.set_object_of_matrix(3, 5, 1, 1); a.set_object_of_matrix(4, 5, 1, 2); a.set_object_of_matrix(5, 5, 1, 0); a.set_object_of_matrix(6, 5, 1, 0);
-	a.set_object_of_matrix(0, 6, 1, 0); a.set_object_of_matrix(1, 6, 1, 0); a.set_object_of_matrix(2, 6, 1, 0); a.set_object_of_matrix(3, 6, 1, 0); a.set_object_of_matrix(4, 6, 1, 0); a.set_object_of_matrix(5, 6, 1, 0); a.set_object_of_matrix(6, 6, 1, 0);
+	if (data1) 
+	{
+		char buff[150];
+		data1.getline(buff, 150);
+		data1.close();
 
-	a.set_object_of_matrix(0, 0, 2, 0); a.set_object_of_matrix(1, 0, 2, 0); a.set_object_of_matrix(2, 0, 2, 0); a.set_object_of_matrix(3, 0, 2, 0); a.set_object_of_matrix(4, 0, 2, 0); a.set_object_of_matrix(5, 0, 2, 0); a.set_object_of_matrix(6, 0, 2, 0);
-	a.set_object_of_matrix(0, 1, 2, 0); a.set_object_of_matrix(1, 1, 2, 2); a.set_object_of_matrix(2, 1, 2, 0); a.set_object_of_matrix(3, 1, 2, 2); a.set_object_of_matrix(4, 1, 2, 0); a.set_object_of_matrix(5, 1, 2, 2); a.set_object_of_matrix(6, 1, 2, 0);
-	a.set_object_of_matrix(0, 2, 2, 0); a.set_object_of_matrix(1, 2, 2, 0); a.set_object_of_matrix(2, 2, 2, 0); a.set_object_of_matrix(3, 2, 2, 1); a.set_object_of_matrix(4, 2, 2, 2); a.set_object_of_matrix(5, 2, 2, 1); a.set_object_of_matrix(6, 2, 2, 0);
-	a.set_object_of_matrix(0, 3, 2, 0); a.set_object_of_matrix(1, 3, 2, 1); a.set_object_of_matrix(2, 3, 2, 0); a.set_object_of_matrix(3, 3, 2, 2);	a.set_object_of_matrix(4, 3, 2, 2); a.set_object_of_matrix(5, 3, 2, 1); a.set_object_of_matrix(6, 3, 2, 0);
-	a.set_object_of_matrix(0, 4, 2, 0); a.set_object_of_matrix(1, 4, 2, 2); a.set_object_of_matrix(2, 4, 2, 0); a.set_object_of_matrix(3, 4, 2, 2);	a.set_object_of_matrix(4, 4, 2, 0); a.set_object_of_matrix(5, 4, 2, 0); a.set_object_of_matrix(6, 4, 2, 0);
-	a.set_object_of_matrix(0, 5, 2, 0); a.set_object_of_matrix(1, 5, 2, 0); a.set_object_of_matrix(2, 5, 2, 0); a.set_object_of_matrix(3, 5, 2, 1);	a.set_object_of_matrix(4, 5, 2, 1); a.set_object_of_matrix(5, 5, 2, 2); a.set_object_of_matrix(6, 5, 2, 0);
-	a.set_object_of_matrix(0, 6, 2, 0); a.set_object_of_matrix(1, 6, 2, 0); a.set_object_of_matrix(2, 6, 2, 0); a.set_object_of_matrix(3, 6, 2, 0);	a.set_object_of_matrix(4, 6, 2, 0); a.set_object_of_matrix(5, 6, 2, 0); a.set_object_of_matrix(6, 6, 2, 0);
+		int q = 0;
+		for(int z = 0; z < 3; z++)
+			for (int y = 0; y < 7; y++)
+				for (int x = 0; x < 7; x++)
+				{
+					a.set_object_of_matrix(x, y, z, int(buff[q] - '0'));
+					q++;
+				}
+		q = 0;
+	}
 
-
-	filter.set_object_of_matrix(0, 0, 0, 1); filter.set_object_of_matrix(1, 0, 0, 1); filter.set_object_of_matrix(2, 0, 0, -1);
-	filter.set_object_of_matrix(0, 1, 0, -1); filter.set_object_of_matrix(1, 1, 0, 0); filter.set_object_of_matrix(2, 1, 0, 1);
-	filter.set_object_of_matrix(0, 2, 0, -1); filter.set_object_of_matrix(1, 2, 0, -1); filter.set_object_of_matrix(2, 2, 0, 0);
-
-	filter.set_object_of_matrix(0, 0, 1, -1); filter.set_object_of_matrix(1, 0, 1, 0); filter.set_object_of_matrix(2, 0, 1, -1);
-	filter.set_object_of_matrix(0, 1, 1, 0); filter.set_object_of_matrix(1, 1, 1, 0); filter.set_object_of_matrix(2, 1, 1, -1);
-	filter.set_object_of_matrix(0, 2, 1, 1); filter.set_object_of_matrix(1, 2, 1, -1); filter.set_object_of_matrix(2, 2, 1, 0);
-
-	filter.set_object_of_matrix(0, 0, 2, 0); filter.set_object_of_matrix(1, 0, 2, 1); filter.set_object_of_matrix(2, 0, 2, 0);
-	filter.set_object_of_matrix(0, 1, 2, 1); filter.set_object_of_matrix(1, 1, 2, 0); filter.set_object_of_matrix(2, 1, 2, 1);
-	filter.set_object_of_matrix(0, 2, 2, 0); filter.set_object_of_matrix(1, 2, 2, -1); filter.set_object_of_matrix(2, 2, 2, 1);
+	//data.set_object_of_matrix(0, 0, 0, 0);
 }
 
 int main()
@@ -51,15 +36,15 @@ int main()
 	{
 		Ten3D data(7, 7, 3, "a", '0');
 		Ten3D filter(3, 3, 3, "filter", '0');
-		Ten3D result(3, 3, 1, "result", '0');
+		//Ten3D result(3, 3, 1, "result", '0');
 		filldata(data, filter);
 
-		data.print();
-		filter.print();
+		//data.print();
+		//filter.print();
 
-		data.convolution(filter, result, 2);
+		//data.convolution(filter, result, 2);
 
-		result.print();
+		//result.print();
 	}
 	catch(length_error& ex) 
 	{
