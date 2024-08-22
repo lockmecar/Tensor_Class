554ffcd07a2800f01e6e6710a2c60c9e3f5d2ed1
nic gor <nic14246@mail.ru>
2024-08-22 18:49:54 +0300

    для фильтера добавлена возможность принимать данные из файла (треб к файлу без пробелов и переносов) возможно добавить загоьловок к файлам

--- Tensor_Class/main.cpp
+++ Tensor_Class/main.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 void filldata(Ten3D &a, Ten3D &filter)
 {
-	ifstream data1("data.txt");
+	ifstream data1("data.txt");//условия для файла числа в одну строку без пробелов и переносов
 
-	if (data1) 
+	if (data1) //добавить к файла заголовок и возможность читать его в коде
 	{
-		char buff[150];
-		data1.getline(buff, 150);
+		char buff_ten[150];
+		data1.getline(buff_ten, 150);
 		data1.close();
 
 		int q = 0;
@@ -20,13 +20,38 @@ void filldata(Ten3D &a, Ten3D &filter)
 			for (int y = 0; y < 7; y++)
 				for (int x = 0; x < 7; x++)
 				{
-					a.set_object_of_matrix(x, y, z, int(buff[q] - '0'));
+					a.set_object_of_matrix(x, y, z, int(buff_ten[q] - '0'));
 					q++;
 				}
 		q = 0;
 	}
 
-	//data.set_object_of_matrix(0, 0, 0, 0);
+	ifstream filter1("filter.txt");
+
+	if (filter1) //добавить к файла заголовок и возможность читать его в коде
+	{
+		char buff_fil[54];
+		filter1.getline(buff_fil, 54);
+		filter1.close();
+
+		int q = 0;
+		for (int z = 0; z < 3; z++)
+			for (int y = 0; y < 3; y++)
+				for (int x = 0; x < 3; x++)
+				{
+					if(buff_fil[q]=='-')
+					{
+						filter.set_object_of_matrix(x, y, z, -int(buff_fil[q+1] - '0'));
+						q+=2;
+					}
+					else 
+					{
+						filter.set_object_of_matrix(x, y, z, int(buff_fil[q] - '0'));
+						q ++;
+					}
+				}
+		q = 0;
+	}
 }
 
 int main()
@@ -36,15 +61,15 @@ int main()
 	{
 		Ten3D data(7, 7, 3, "a", '0');
 		Ten3D filter(3, 3, 3, "filter", '0');
-		//Ten3D result(3, 3, 1, "result", '0');
+		Ten3D result(3, 3, 1, "result", '0');
 		filldata(data, filter);
 
-		//data.print();
-		//filter.print();
+		data.print();
+		filter.print();
 
-		//data.convolution(filter, result, 2);
+		data.convolution(filter, result, 2);
 
-		//result.print();
+		result.print();
 	}
 	catch(length_error& ex) 
 	{
